6c44ceabec6e96f7fe6c3217afed11236bded603
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-09 20:26:49 -0300

    Update enrollments.module.ts

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnrollmentsController } from './enrollments.controller';
 import { EnrollmentsService } from './enrollments.service';
 import { EnrollmentEntity } from './enrollment.entity';
+import { RoomEntity } from '../rooms/room.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EnrollmentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      EnrollmentEntity,
+      RoomEntity,
+    ]),
+  ],
   controllers: [EnrollmentsController],
   providers: [EnrollmentsService],
 })
