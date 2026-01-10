1c97fa4107b966125024e5b88191729bece0acd2
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-09 22:13:09 -0300

    Update rooms.module.ts

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -4,9 +4,12 @@ import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 import { RoomEntity } from './room.entity';
 import { EnrollmentEntity } from '../enrollments/enrollment.entity';
+import { UserEntity } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoomEntity, EnrollmentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([RoomEntity, EnrollmentEntity, UserEntity]),
+  ],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
