9fbfe3d3fa63ea5deaf090b7268a1e693d5f0fb4
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-10 15:05:04 -0300

    Update enrollments.service.ts

--- src/enrollments/enrollments.service.ts
+++ src/enrollments/enrollments.service.ts
@@ -1,42 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { EnrollmentEntity } from './enrollment.entity';
-import { RoomEntity } from '../rooms/room.entity';
 
 @Injectable()
 export class EnrollmentsService {
   constructor(
     @InjectRepository(EnrollmentEntity)
     private readonly enrollmentRepo: Repository<EnrollmentEntity>,
-
-    @InjectRepository(RoomEntity)
-    private readonly roomRepo: Repository<RoomEntity>,
   ) {}
 
-  async joinByCode(code: string, studentId: string) {
-   
-    const room = await this.roomRepo.findOne({ where: { code } });
-
-    if (!room) {
-      throw new NotFoundException('Sala não encontrada');
-    }
-
-    const alreadyEnrolled = await this.enrollmentRepo.findOne({
-      where: { roomId: room.id, studentId },
+  async enroll(roomId: string, studentId: string) {
+    const exists = await this.enrollmentRepo.findOne({
+      where: { roomId, studentId },
     });
 
-    if (alreadyEnrolled) {
-      return room;
+    if (exists) {
+      throw new BadRequestException('Aluno já matriculado nesta sala');
     }
 
     const enrollment = this.enrollmentRepo.create({
-      roomId: room.id,
+      roomId,
       studentId,
     });
 
-    await this.enrollmentRepo.save(enrollment);
-
-    return room;
+    return this.enrollmentRepo.save(enrollment);
   }
 }
