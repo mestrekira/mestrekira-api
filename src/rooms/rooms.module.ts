b2c7eb36c653dfec6e55fe45944a42f44fad9b3a
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-09 21:11:52 -0300

    Update rooms.module.ts

--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RoomEntity } from './room.entity';
-import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
+import { RoomsService } from './rooms.service';
+import { RoomEntity } from './room.entity';
+import { EnrollmentEntity } from '../enrollments/enrollment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoomEntity])],
-  providers: [RoomsService],
+  imports: [TypeOrmModule.forFeature([RoomEntity, EnrollmentEntity])],
   controllers: [RoomsController],
+  providers: [RoomsService],
 })
 export class RoomsModule {}
