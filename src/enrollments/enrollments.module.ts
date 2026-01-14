ff0070123a4d440b30d974bb02c6f06db506c75e
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-14 20:06:22 -0300

    Update enrollments.module.ts

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -4,12 +4,14 @@ import { EnrollmentsController } from './enrollments.controller';
 import { EnrollmentsService } from './enrollments.service';
 import { EnrollmentEntity } from './enrollment.entity';
 import { RoomEntity } from '../rooms/room.entity';
+import { UserEntity } from '../users/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       EnrollmentEntity,
       RoomEntity,
+      UserEntity, // 🔥 necessário para listar alunos com nome/email
     ]),
   ],
   controllers: [EnrollmentsController],
