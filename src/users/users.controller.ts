ad2bbbf6c654aaa646baee8121561ff150d7f969
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-15 18:27:02 -0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Patch, Delete, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -38,9 +39,27 @@ async login(@Body('email') email: string, @Body('password') password: string) {
   };
 }
 
+@Patch(':id')
+update(
+  @Param('id') id: string,
+  @Body() body: { email?: string; password?: string },
+) {
+  return this.usersService.updateUser(id, body.email, body.password);
+}
 
+@Delete(':id')
+remove(@Param('id') id: string) {
+  return this.usersService.removeUser(id);
+}
+
+@Get(':id')
+findOne(@Param('id') id: string) {
+  return this.usersService.findById(id);
+}
+  
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 }
+
