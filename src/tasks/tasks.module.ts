a7e86b6c7996347d36a711eff8ba9264c81410ac
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-01-16 17:09:03 -0300

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+
 import { TaskEntity } from './task.entity';
 import { EnrollmentEntity } from '../enrollments/enrollment.entity';
+import { EssayEntity } from '../essays/essay.entity'; // ✅ IMPORT QUE ESTAVA FALTANDO
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskEntity, EnrollmentEntity, EssayEntity])
+    TypeOrmModule.forFeature([TaskEntity, EnrollmentEntity, EssayEntity]),
   ],
   controllers: [TasksController],
   providers: [TasksService],
 })
 export class TasksModule {}
-
