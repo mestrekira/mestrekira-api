cb5b9bc4eab060d7661ca8d807bb66e109e6b563
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-05 20:33:22 -0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+
 import { UserEntity } from './user.entity';
+import { EssayEntity } from '../essays/essay.entity';
+import { RoomEntity } from '../rooms/room.entity';
+import { EnrollmentEntity } from '../enrollments/enrollment.entity';
+import { TaskEntity } from '../tasks/task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      EssayEntity,
+      RoomEntity,
+      EnrollmentEntity,
+      TaskEntity,
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
