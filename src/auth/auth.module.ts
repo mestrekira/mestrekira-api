556f5cba675d6721ac9b9a79909b7ae81bce966d
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-09 19:04:47 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserEntity } from '../users/user.entity';
+import { UsersModule } from '../users/users.module';
 import { MailModule } from '../mail/mail.module';
 
 import { AuthController } from './auth.controller';
@@ -10,10 +11,11 @@ import { AuthService } from './auth.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity]),
+    UsersModule,  // ✅ necessário (AuthService usa UsersService)
     MailModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService], // ✅ para o UsersController poder usar
+  exports: [AuthService],
 })
 export class AuthModule {}
