f691366bc7e3772aad70f987f0849fa4e246156b
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-09 00:14:56 -0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Post, Query, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly auth: AuthService) {}
 
-  // link que chega no e-mail:
-  // GET https://mestrekira-api.onrender.com/auth/verify-email?token=...
+  // Link do e-mail: /auth/verify-email?token=...
   @Get('verify-email')
-  verifyEmail(@Query('token') token: string) {
+  async verifyEmail(@Query('token') token: string) {
+    // depois, se quiser, podemos retornar HTML bonitinho ou redirecionar.
     return this.auth.verifyEmail(token);
+  }
+
+  // Reenvio: POST /auth/resend-verification { "email": "..." }
+  @Post('resend-verification')
+  async resend(@Body('email') email: string) {
+    return this.auth.resendVerification(email);
   }
 }
