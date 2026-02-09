623071356f98c0787099653efe2319075d5d9265
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-08 22:15:15 -0300

    Update mail.module.ts

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController, MailPublicController } from './mail.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])], // ou só TypeOrmModule se você usa DataSource direto
   controllers: [MailController, MailPublicController],
   providers: [MailService],
   exports: [MailService],
