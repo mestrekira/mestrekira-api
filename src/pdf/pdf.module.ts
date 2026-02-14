fcd508bb454893386a911ed0fb07c1e79bbbfec3
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-13 21:26:48 -0300

    Update pdf.module.ts

--- src/pdf/pdf.module.ts
+++ src/pdf/pdf.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PdfService } from './pdf.service';
 import { PdfController } from './pdf.controller';
 
-// ✅ ajuste imports dos seus módulos reais:
 import { EssaysModule } from '../essays/essays.module';
 import { RoomsModule } from '../rooms/rooms.module';
 import { TasksModule } from '../tasks/tasks.module';
 import { UsersModule } from '../users/users.module';
 
+import { UserEntity } from '../users/user.entity';
+
 @Module({
-  imports: [EssaysModule, RoomsModule, TasksModule, UsersModule],
+  imports: [
+    EssaysModule,
+    RoomsModule,
+    TasksModule,
+    UsersModule,
+
+    // ✅ necessário para usar @InjectRepository(UserEntity)
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   controllers: [PdfController],
   providers: [PdfService],
 })
