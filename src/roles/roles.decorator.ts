633a53be1845ceed8dcdc180d8715a1429915c92
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-24 17:11:24 -0300

    Delete src/roles directory

--- src/roles/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Array<'student' | 'professor' | 'school'>) =>
-  SetMetadata(ROLES_KEY, roles);
