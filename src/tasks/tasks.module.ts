790d8323238ac60f345ec8e7797c28cbe3590dad
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-25 18:34:00 -0300

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -7,14 +7,19 @@ import { TasksService } from './tasks.service';
 import { TaskEntity } from './task.entity';
 import { EnrollmentEntity } from '../enrollments/enrollment.entity';
 import { EssayEntity } from '../essays/essay.entity';
+import { RoomEntity } from '../rooms/room.entity'; // ✅ IMPORTANTE
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskEntity, EnrollmentEntity, EssayEntity]),
+    TypeOrmModule.forFeature([
+      TaskEntity,
+      EnrollmentEntity,
+      EssayEntity,
+      RoomEntity, // ✅ necessário se TasksService injeta RoomEntityRepository
+    ]),
   ],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [TasksService],
 })
 export class TasksModule {}
-
