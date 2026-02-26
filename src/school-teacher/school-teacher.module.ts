c51540d2b4da0156d20a68f97fc763e2e4777729
Leildo Gonçalves <mestrekiraoficial@gmail.com>
2026-02-25 21:53:37 -0300

    Update school-teacher.module.ts

--- src/school-teacher/school-teacher.module.ts
+++ src/school-teacher/school-teacher.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
 
-import { UserEntity } from '../users/user.entity';
 import { SchoolTeacherInviteEntity } from './school-teacher-invite.entity';
+import { UserEntity } from '../users/user.entity';
+
 import { SchoolTeacherController } from './school-teacher.controller';
 import { SchoolTeacherService } from './school-teacher.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserEntity, SchoolTeacherInviteEntity]),
-    JwtModule.register({}),
-  ],
+  imports: [TypeOrmModule.forFeature([SchoolTeacherInviteEntity, UserEntity])],
   controllers: [SchoolTeacherController],
   providers: [SchoolTeacherService],
+  exports: [SchoolTeacherService],
 })
 export class SchoolTeacherModule {}
